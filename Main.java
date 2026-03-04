c75d4c2c5aac0f65df0c287eda0cf1282d625247
Divaynshi <divyanshi3240804@gmail.com>
2026-03-04 18:20:31 +0530

    Day 3 - scanner program

--- Main.java
+++ Main.java
@@ -1,15 +1,22 @@
 
 
 
-public class main{
-    public static void main (String[ ]args)
-    {
-       int marks = 75;
-       if (marks>40){
-        System.out.println("pass");
-       }
-         else{
-            System.out.println("fail");
-         }
-}
+import java.util.Scanner;
+ 
+public class Main {
+   public static void main (String[]args)
+   {
+      Scanner sc = new 
+      Scanner(System.in);
+            System.out.println("enter your marks:");
+            int marks = sc.nextInt();
+            if (marks >=90) {
+      System.out.println("Excellent");
+            } else if (marks >= 40 ) {
+                  System.out.println("Pass");
+            } else {
+               System.out.println("Fail");
+            }
+            sc.close();
+   }
 }
